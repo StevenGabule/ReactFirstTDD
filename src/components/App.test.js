e7c3ecd05f0fc6cf718167a65fe4cec87c89b16b
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-03-10 01:20:13 +0800

    render the giftlist in the app

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,11 +4,17 @@ import App from './App'
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
-const wrapper = shallow(<App/>);
+const app = shallow(<App/>);
 
 it('should renders app', () => {
-    expect(wrapper).toMatchSnapshot();
+    expect(app).toMatchSnapshot();
 });
-// describe(<App/>, () => {
-//
-// });
+
+it('should initializes the state with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+});
+
+it('should add a new gift to state when clicking the add first button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.state().gifts).toEqual([{id: 1}])
+});
